005586e989147a892c9c722ae9b0d3499b3f681a
Meryem <71948548+MeryemEroglu@users.noreply.github.com>
2023-12-07 18:20:22 +0300

    Deneme commiti

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,7 +1,5 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-  
-  </q-page>
+  <q-page class="row items-center justify-evenly"> </q-page>
 </template>
 
 <script setup lang="ts">
@@ -12,26 +10,26 @@ import { ref } from 'vue';
 const todos = ref<Todo[]>([
   {
     id: 1,
-    content: 'ct1'
+    content: 'component1',
   },
   {
     id: 2,
-    content: 'ct2'
+    content: 'ct2',
   },
   {
     id: 3,
-    content: 'ct3'
+    content: 'ct3',
   },
   {
     id: 4,
-    content: 'ct4'
+    content: 'ct4',
   },
   {
     id: 5,
-    content: 'ct5'
-  }
+    content: 'ct5',
+  },
 ]);
 const meta = ref<Meta>({
-  totalCount: 1200
+  totalCount: 1200,
 });
 </script>
