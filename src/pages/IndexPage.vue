b0cfbd29efc9bedd55bbbbc49197b21b32d6fc7c
ArwensChild <75691062+BerkantEruysal@users.noreply.github.com>
2023-12-07 18:34:52 +0300

    Delete unused files

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,35 +1,7 @@
 <template>
-  <q-page class="row items-center justify-evenly"> </q-page>
+  <div>
+    <p>deneme</p>
+  </div>
 </template>
 
-<script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'component1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-]);
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
-</script>
+<script setup lang="ts"></script>
