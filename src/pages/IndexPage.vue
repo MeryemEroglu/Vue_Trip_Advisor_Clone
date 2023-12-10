f84ba1212f987e3b231ecf6eade84bc79d4e93c4
Meryem <71948548+MeryemEroglu@users.noreply.github.com>
2023-12-10 16:12:22 +0300

    Add components to index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,28 @@
 <template>
   <div>
-    <NavBar></NavBar>
+    <NavBar
+      @openLocalizationModal="
+        () => {
+          isLocalizationModalActive = true;
+        }
+      "
+    ></NavBar>
+    <br />
+    <br />
+    <LocalizationOptions
+      @closeLocalizationModal="
+        () => {
+          isLocalizationModalActive = false;
+        }
+      "
+      v-if="isLocalizationModalActive == true"
+    ></LocalizationOptions>
   </div>
 </template>
 
 <script setup lang="ts">
 import NavBar from 'src/components/NavBar.vue';
+import LocalizationOptions from 'src/components/LocalizationOptions.vue';
+import { ref } from 'vue';
+const isLocalizationModalActive = ref<boolean>(false);
 </script>
