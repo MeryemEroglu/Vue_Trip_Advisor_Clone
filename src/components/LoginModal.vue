d461ad0755cbc9f45f358cee44edeac7541115a6
Meryem <71948548+MeryemEroglu@users.noreply.github.com>
2023-12-12 00:26:38 +0300

    Create login modal component template

---
+++ src/components/LoginModal.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="loginModal-container">
+    <div class="loginModal-innerCoontainer">
+      <button @click="$emit('closeLocalizationModal')">
+        <i class="ri-close-line"></i>
+      </button>
+      <img src="../assets/logo-white.svg" alt="Logo" />
+      <h2>Tripadvisor’ın en iyilerini görüntülemek için giriş yapın.</h2>
+      <div class="loginOption-button">
+        <img
+          class="loginOption-icon"
+          src="../assets/googleLogo.png"
+          alt="google"
+        />
+        <span>Google ile devam et</span>
+      </div>
+      <div class="loginOption-button">
+        <i class="ri-mail-line loginOption-icon"></i>
+        <span>E-posta ile devam et</span>
+      </div>
+      <div class="userAgreementContainer">
+        <p>
+          Devam ettiğiniz takdirde
+          <a href="https://www.tripadvisor.com.tr/pages/terms.html?locale=tr-TR"
+            >Kullanım Koşullarımızı</a
+          >
+          kabul etmiş ve
+          <a
+            href="https://www.tripadvisor.com.tr/pages/privacy.html?locale=tr-TR"
+            >Gizlilik ve Çerezlerle İlgili Bildirimimizi</a
+          >
+          okuduğunuzu onaylamış sayılırsınız.
+        </p>
+        <p>
+          Bu site, reCAPTCHA koruması altında olup Google
+          <a href="https://policies.google.com/privacy">Gizlilik Politikası</a>
+          ve <a href="https://policies.google.com/terms">Hizmet Şartları</a> 'na
+          tabidir.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.loginModal-container {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(76, 76, 76, 0.586);
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -17,12 +17,16 @@
       "
       v-if="isLocalizationModalActive == true"
     ></LocalizationOptions>
+    <br />
+    <br />
+    <LoginModal></LoginModal>
   </div>
 </template>
 
 <script setup lang="ts">
 import NavBar from 'src/components/NavBar.vue';
 import LocalizationOptions from 'src/components/LocalizationOptions.vue';
+import LoginModal from 'src/components/LoginModal.vue';
 import { ref } from 'vue';
 const isLocalizationModalActive = ref<boolean>(false);
 </script>
