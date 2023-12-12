e0a2272cd7be0a4d4a839b2be47537d7ea7fd8cb
ArwensChild <75691062+BerkantEruysal@users.noreply.github.com>
2023-12-12 20:19:40 +0300

    Create ReviewList Component

--- src/components/Reviews/ReviewList.vue
+++ src/components/Reviews/ReviewList.vue
@@ -1,11 +1,39 @@
 <script setup lang="ts">
 import TheReview from 'src/components/Reviews/TheReview.vue';
+import { ref } from 'vue';
+import { defineProps } from 'vue';
+
+interface review {
+  score: number;
+  likeCount: number;
+  isUserLiked: boolean;
+  createdAt: string;
+  title: string;
+  mainText: string;
+  userName: string;
+  userLocation: string;
+  userContributionCount: number;
+  imageUrl?: string;
+}
+
+const props = defineProps<{ reviews: review[] }>();
 </script>
 
 <template>
-  <TheReview></TheReview>
-  <TheReview></TheReview>
-  <TheReview></TheReview>
+  <TheReview
+    v-for="review in props.reviews"
+    :key="review.title"
+    :score="review.score"
+    :like-count="review.likeCount"
+    :is-user-liked="review.isUserLiked"
+    :created-at="review.createdAt"
+    :title="review.title"
+    :main-text="review.mainText"
+    :user-name="review.userName"
+    :user-location="review.userLocation"
+    :user-contribution-count="review.userContributionCount"
+    :image-url="review.imageUrl"
+  ></TheReview>
 </template>
 
 <style scoped></style>
